e837eb1a5250863902b4a2fca6c89a5a938d339b
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-01-01 08:00:25 +0000

    Adding the first cut of the file service.

---
+++ service/service.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import json
+import hashlib
+import os.path
+import wsgiref.simple_server
+
+class Index:
+
+  def __init__(self, directory):
+
+    self.directory = directory
+    self.index = {}
+
+    # List the directory.
+    for (dirpath, dirnames, filenames) in os.walk(directory):
+      for filename in filenames:
+        md5 = hashlib.md5()
+        md5.update(filename)
+        (name, ext) = os.path.splitext(filename)
+        path = os.path.join(dirpath, filename)
+        path = path.replace(directory, "", 1)
+        identifier = md5.hexdigest()
+        self.index[identifier] = {'id': identifier, 'name': name, 'path': path}
+
+  def items(self):
+    return map(lambda x: x[1], self.index.items())
+
+  def item(self, identifier):
+    return self.index[identifier];
+
+  def ids(self):
+    return map(lambda x: x[0], self.index.items())
+
+
+def application(directory, environ, start_response):
+
+  index = Index(directory)
+
+  path = wsgiref.util.shift_path_info(environ)
+  if (path == ""):
+
+    response_body = json.dumps(index.items())
+    status = '200 OK'
+    headers = [('Content-Type', 'application/json'),
+               ('Content-Length', str(len(response_body)))]
+    start_response(status, headers)
+    return [response_body]
+
+  else:
+
+    item = index.item(path)
+    filepath = os.path.join(directory, item['path'])
+    status = '200 OK'
+    headers = [('Content-Type', 'image/jpeg'),
+               ('Content-Length', str(os.path.getsize(filepath)))]
+    start_response(status, headers)
+    return open(filepath, "rb").read()
+
+
+def main():
+
+  parser = argparse.ArgumentParser(description = "Run a small web service exposing all the files within the specified directory.")
+  parser.add_argument("directory", help = "Directory to index")
+  options = parser.parse_args()
+
+  app = lambda environ, start_response: application(options.directory, environ, start_response)
+  httpd = wsgiref.simple_server.make_server(
+    '0.0.0.0',
+    8051,
+    app
+    )
+
+  httpd.serve_forever()
+
+
+if __name__ == '__main__':
+  main()
